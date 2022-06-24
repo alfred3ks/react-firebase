75d70586bd927ba5a8e61f569b6d4c53a956d78f
Alfredo Sánchez <alfred3ks@gmail.com>
2022-06-24 17:00:09 +0200

    Vemos los metodos de como crear un app de react usando la linea de comando.

---
+++ 2-create-react-app/notas-clase.js
@@ -0,0 +1,28 @@
+/*
+
+Para instalar una aplicacion de React usando la linea de comando: La forma oficial dada por facebook y que vemos en la documentacion:
+
+https://es.reactjs.org/docs/create-a-new-react-app.html
+
+Esra forma  de creara un app de React por linea de comando se llama create react app. Esta forma de arranque de React usa webpack como empaquetador de la app.
+
+Creamos la app:
+
+npx create-react-app aprendiendo_react
+
+Con vite tambien podemos generar una app de React:
+npm init vite@latest
+
+Vale he realizado limpia de archivos para quitar todo lo quq ponen desde de principio para poder montar nuestra app.
+
+De aqui en adelante no lo hare con el comando de la documentacion, o hare con vite porque es mas limpio el proyecto.
+
+npm init vite@latest
+
+Seguir las instrucciones y listo.
+
+A partir de ahora trabajaremos con el proyecto react_app
+
+*/
+
+
---
+++ 2-create-react-app/react_app/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export function App() {
+  return (
+    <div>
+      <h1>Hello World and React with vite...</h1>
+    </div>
+  )
+}
+
+
