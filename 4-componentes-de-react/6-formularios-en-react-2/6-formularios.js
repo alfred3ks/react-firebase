c77150a10f5e14a869ae999b26fe13c9118a76c8
Alfredo Sánchez <alfred3ks@gmail.com>
2022-06-25 16:51:21 +0200

    Formularios 2, como pasar por props funciones.

---
+++ 4-componentes-de-react/6-formularios-en-react-2/6-formularios.js
@@ -0,0 +1,17 @@
+/*
+
+Continuamos trabajado con nuestrp formulario, ya hemos visto como capturar los datos del input usando useState.
+
+Ahora vamos a terminar el formulario para con una pequeña validacion poder acceder al contenido.
+
+Aqui es interesante ver los dos metodos que recibe los input:
+
+
+value={usuario}
+onChange={onChange}
+
+Y como se pasa una funcion que se ejecuta en App.jsx que es cambiar la sesion y como la pasamos por props al formulario.
+
+Aqui podriamos hacer un ejercicio donde los datos que se capturan del formulario se muestre al usuario cuando se autentifique.
+
+*/
